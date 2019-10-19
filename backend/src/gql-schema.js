71c716f925c20a4b9dfdb9d474c4481a495838c9
meebs <alex@takayamasan.com>
2019-10-19 13:17:28 -0400

    added schema

--- backend/src/gql-schema.js
+++ backend/src/gql-schema.js
@@ -1,10 +1,17 @@
-import { buildSchema } from "graphql";
-
 /**
  * Part of the Poo-NU repository.
  * Contains the Schema for request formats.
  */
+
+import { buildSchema } from "graphql";
+
+
  
+ /**
+  * The Request format for GraphQL. Must exist.
+  * @param getBathrooms list of bathrooms
+  * @param getBuildings list of buildings
+  */
 export const Request = buildSchema(`
   type Query {
     getBathrooms: [Bathroom!]!
@@ -12,19 +19,44 @@ export const Request = buildSchema(`
   }
 `);
 
+/**
+ * A Bathroom is a bathroom.
+ * @param bathroom_id a unique id
+ * @param building_id the id of the building this bathroom is in
+ * @param name the name 
+ * @param description the description 
+ * @param floor the floor number this bathroom is on
+ * @param male is this a male bathroom?
+ * @param female is this a female bathroom?
+ * @param all_gender is this bathroom allgender?
+ * @param handicap_accessible is this bathroom handicap accessible?
+ */
 export const Bathroom = buildSchema(`
   type Bathroom {
+    bathroom_id: Int!
+    building_id: Int!
     name: String!
-    building: Building!
     description: String!
     floor: Int!
+    male: Boolean!
+    female: Boolean!
+    all_gender: Boolean!
+    handicap_accessible: Boolean!
   }
 `);
 
+/**
+ * A Building is a building, with bathroom.
+ * @param id the unique id 
+ * @param name the name 
+ * @param longitude the longitude
+ * @param latitude the latitude
+ */
 export const Building = buildSchema(`
   type Building {
+    id: Int!
+    name: String!
     longitude: Float!
     latitude: Float!
-    rooms: [Bathroom!]!
   }
 `);
