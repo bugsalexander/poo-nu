deabed978457673ac1535396e49e0801863362bd
meebs <alex@takayamasan.com>
2019-10-19 17:36:24 -0400

    added nearest building

--- backend/src/sql-connect.js
+++ backend/src/sql-connect.js
@@ -3,4 +3,42 @@
  * This file contains scripts for talking to the mySQL database, which should also be running on this machine.
  */
 
+import { connect } from "mysql";
+
 // scripts go here 
+
+/**
+ * Produces the nearest buildings.
+ */
+function nearestxbuildings(database, longitude, latitude, count) {
+  return database.connect(function(err) {
+    if (err) {
+      console.log(err);
+      return {};
+    } else {
+      return findNearest(database, longitude, latitude, count);
+    }
+  })
+}
+
+/**
+ * @param {*} database the database
+ * @param {number} longitude 
+ * @param {number} latitude 
+ * @param {number} count 
+ */
+function findNearest(database, latitude, longitude, count) {
+
+  return database.query(""
+  + "SELECT *, ROUND(((SQRT(POWER((building_latitude - " 
+  + latitude 
+  + "), 2) POWER((building_longitude - " 
+  + longitude 
+  + "), 2)) * 10000 / 90) * 3280.4), 0) as ft FROM Building join Bathroom using (building_id) order by ft asc limit " 
+  + count 
+  + ";", 
+    function (err, result, fields) {
+    if (err) throw err;
+    return result;
+  });
+}
