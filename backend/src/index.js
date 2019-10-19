0c864a2eda622f0933400ed8951f7cc491a07e7f
meebs <alex@takayamasan.com>
2019-10-19 17:35:59 -0400

    attempts to connect to database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,23 @@
 import { ApolloServer } from 'apollo-server';
 import { typeDefs } from './gql-schema';
-import { resolvers } from './gql-produce';
+import { gql_resolver } from './gql-produce';
+import { createConnection } from 'mysql';
+import { readFileSync } from 'fs';
+
+/**
+ * This file is part of Poo-NU
+ */
+ 
+// tries to connect to the database
+const login = JSON.parse(readFileSync("../database/mysql_login.txt"));
+const database = createConnection(login);
+
+// runs the apollo server
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, gql_resolver(database) 
+});
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }) => {
