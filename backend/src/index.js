cc58ab3a9698959592588bf6a517603d43dba7e7
meebs <alex@takayamasan.com>
2019-10-19 14:21:41 -0400

    built server

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+import { ApolloServer } from 'apollo-server';
+import { typeDefs } from './gql-schema';
+import { resolvers } from './gql-produce';
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
