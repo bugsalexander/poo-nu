d792b22807012d3f3900be9798fbe1b23b5cd9e2
Stanley Liu <liustan24@gmail.com>
2019-10-20 05:37:33 -0400

    Homepage redesign

--- front-end/src/components/BathroomPanel.js
+++ front-end/src/components/BathroomPanel.js
@@ -1,44 +1,66 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import {Text, View, StyleSheet, Button, Card} from 'react-native';
-import BathroomScreen from '../pages/BathroomScreen';
-import { Actions } from 'react-native-router-flux';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  Card,
+  TouchableOpacity
+} from "react-native";
+import { Actions } from "react-native-router-flux";
+import { Rating, AirbnbRating } from "react-native-elements";
 
 export default class BathroomPanel extends Component {
-    static navigationOptions = {
-        title: 'Pick a Bathroom'
-    };
+  static navigationOptions = {
+    title: "Pick a Bathroom"
+  };
 
-    // TODO: produce bathroom images dependent on type of bathroom
-    // produces a bathroom image depending on the type of bathroom to be displayed.
-    determineBathroomType(b) {
+  // TODO: produce bathroom images dependent on type of bathroom
+  // produces a bathroom image depending on the type of bathroom to be displayed.
+  determineBathroomType(b) {}
 
-    }
-
-    render() {
-        return (
-        <View style = {{alignItems: 'center', width: '100%'}} >
-            <Text onPress ={() => {Actions.info({lastPos: this.props.lastPos, bathroomName: this.props.bathroomName})}} style = {styles.titleText}> {this.props.bathroomName}</Text>
-            <Text>{this.props.bathroomAddress}</Text>
-            <View style={styles.line} />
+  render() {
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          Actions.info({
+            lastPos: this.props.lastPos,
+            bathroomName: this.props.bathroomName
+          });
+        }}
+      >
+        <View style={{ alignItems: "left", width: "100%", paddingLeft: 16 }}>
+          <Text style={styles.titleText}> {this.props.bathroomName}</Text>
+          <Rating
+            style={{ paddingLeft: 4, marginTop: 4, marginBottom: 4 }}
+            imageSize={20}
+            readonly
+            startingValue={3}
+          />
+          <View style={{ paddingLeft: 6, flexDirection: "row", flex: 1 }}>
+            <Text style={{ flex: 0.4 }}>{this.props.bathroomAddress}</Text>
+            <Text style={{ flex: 0.3 }}>{this.props.genderText}</Text>
+            <Text style={{ flex: 0.3 }}>{this.props.handicapText}</Text>
+          </View>
         </View>
-        );
-    }
+      </TouchableOpacity>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    baseText: {
-        fontFamily: 'Avenir',
-    },
-    titleText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    line: {
-        marginTop: 12,
-        marginBottom: 12,
-        borderBottomColor: "#EBEBEB",
-        borderBottomWidth: 1
-      }
+  baseText: {
+    fontFamily: "Avenir"
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  line: {
+    marginTop: 12,
+    marginBottom: 12,
+    borderBottomColor: "#EBEBEB",
+    borderBottomWidth: 1
+  }
 });
-
--- front-end/src/pages/BathroomPage.js
+++ front-end/src/pages/BathroomPage.js
@@ -1,112 +1,242 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-import {SafeAreaView, ActivityIndicator, Text, Button, View, StyleSheet, ScrollView, SectionList } from 'react-native';
-import BathroomPanel from '../components/BathroomPanel';
-import GoButton from '../components/GoButton';
-import axios from 'axios';
-const list = [ 
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'},
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'},
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'},
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'},
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'},
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'},
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'},
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'},
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'},
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'},
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'},
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'},
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'},
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'},
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'},
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'},
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'},
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'},
-    {bathroomName:'West Village H: Floor 1', bathroomAddress: '291 St. Botolph St.'}];
-
+import {
+  SafeAreaView,
+  ActivityIndicator,
+  Text,
+  Button,
+  View,
+  StyleSheet,
+  ScrollView,
+  SectionList
+} from "react-native";
+import BathroomPanel from "../components/BathroomPanel";
+import GoButton from "../components/GoButton";
+import axios from "axios";
+const list = [
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  },
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  },
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  },
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  },
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  },
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  },
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  },
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  },
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  },
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  },
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  },
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  },
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  },
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  },
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  },
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  },
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  },
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  },
+  {
+    bathroomName: "West Village H: Floor 1",
+    bathroomAddress: "291 St. Botolph St.",
+    genderText: "All-Gender",
+    handicapText: "Handicap"
+  }
+];
 
 export default class BathroomPage extends Component {
-    state = {
-        initialPosition: 'unknown',
-        lastPosition: 'unknown', 
-        query: null   
-    }
+  state = {
+    initialPosition: "unknown",
+    lastPosition: "unknown",
+    query: null
+  };
 
-    watchID = null;
-    componentDidMount = () => {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-            const initialPosition = JSON.stringify(position);
-            this.setState({ initialPosition });
-            },
-            (error) => alert(error.message),
-            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-        );
-        this.watchID = navigator.geolocation.watchPosition((position) => {
-            const lastPosition = JSON.stringify(position);
-            this.setState({ lastPosition });
-        });
+  watchID = null;
+  componentDidMount = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const initialPosition = JSON.stringify(position);
+        this.setState({ initialPosition });
+      },
+      error => alert(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+    this.watchID = navigator.geolocation.watchPosition(position => {
+      const lastPosition = JSON.stringify(position);
+      this.setState({ lastPosition });
+    });
 
-        const query = this.getNearestBathrooms(20);
-        this.setState({
-          query
-        });
+    const query = this.getNearestBathrooms(20);
+    this.setState({
+      query
+    });
+  };
+  componentWillUnmount = () => {
+    navigator.geolocation.clearWatch(this.watchID);
+  };
 
-    }
-    componentWillUnmount = () => {
-        navigator.geolocation.clearWatch(this.watchID);
-    }
-    
-    getNearestBathrooms = (numBathrooms) => {
-        axios({
-            url:'localhost://3000 or something',
-            method: 'get',
-            data: {
-                query: "query {"
-                    + "getNearestBathrooms(lat: ${this.state.lat}, long: ${this.state.long}, count:${numBathrooms}) {"
-                    +  "bathroom_id"
-                    +  "building_id"
-                    +  "name"
-                    +  "building_name"
-                    +  "description"
-                    +  "floor"
-                    +  "male"
-                    +  "female"
-                    +  "all_gender"
-                    +  "handicap_accessible"
-                  }
-        }).then((result) => {
-            console.log(result.data);
-            return result.data;
-        });
+  getNearestBathrooms = numBathrooms => {
+    axios({
+      url: "localhost://3000 or something",
+      method: "get",
+      data: {
+        query:
+          "query {" +
+          "getNearestBathrooms(lat: ${this.state.lat}, long: ${this.state.long}, count:${numBathrooms}) {" +
+          "bathroom_id" +
+          "building_id" +
+          "name" +
+          "building_name" +
+          "description" +
+          "floor" +
+          "male" +
+          "female" +
+          "all_gender" +
+          "handicap_accessible"
       }
+    }).then(result => {
+      console.log(result.data);
+      return result.data;
+    });
+  };
 
-    render() {
-        return (
-            <View style={{ marginTop: 40, width: '100%', height:'100%'}}>
-            <ScrollView style={{flex: 0.8}} horizontal = {false} showsHorizontalScrollIndicator={true} showsVerticalScrollIndicator = {false}>
-                {list.map((elem, i) => {return <BathroomPanel lastPos = {this.state.lastPosition} navigation = {this.props.navigation} key = {i} bathroomName = {elem.bathroomName} bathroomAddress = {elem.bathroomAddress} />})}
-            </ScrollView>
-            <GoButton lastPos = {this.state.lastPos} style ={styles.button}/>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={{ marginTop: 40, width: "100%", height: "100%" }}>
+        <ScrollView
+          style={{ flex: 0.8 }}
+          horizontal={false}
+          showsHorizontalScrollIndicator={true}
+          showsVerticalScrollIndicator={false}
+        >
+          {list.map((elem, i) => {
+            return (
+              <View>
+                <BathroomPanel
+                  lastPos={this.state.lastPosition}
+                  navigation={this.props.navigation}
+                  key={i}
+                  bathroomName={elem.bathroomName}
+                  bathroomAddress={elem.bathroomAddress}
+                  genderText={elem.genderText}
+                  handicapText={elem.handicapText}
+                />
+                <View style={styles.line} />
+              </View>
+            );
+          })}
+        </ScrollView>
+        <GoButton lastPos={this.state.lastPos} style={styles.button} />
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create ({
-    item: {
-       flexDirection: 'row',
-       justifyContent: 'space-between',
-       alignItems: 'center',
-       padding: 30,
-       margin: 2,
-       borderColor: '#2a4944',
-       borderWidth: 1,
-       backgroundColor: '#d2f7f1'
-    },
-    button: {
-        flex: 0.2, 
-        marginBottom: 0
-    }
- })
+const styles = StyleSheet.create({
+  item: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 30,
+    margin: 2,
+    borderColor: "#2a4944",
+    borderWidth: 1,
+    backgroundColor: "#d2f7f1"
+  },
+  button: {
+    flex: 0.2,
+    marginBottom: 0
+  },
+  line: {
+    marginTop: 12,
+    marginBottom: 12,
+    borderBottomColor: "#EBEBEB",
+    borderBottomWidth: 1
+  }
+});
