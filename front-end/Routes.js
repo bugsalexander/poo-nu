c8148a2d908562046a400e20264efdacc49fc3b8
jakechvatal <jakechvatal@github.com>
2019-10-19 23:45:10 -0400

    better style than before

--- front-end/Routes.js
+++ front-end/Routes.js
@@ -1,26 +1,25 @@
-import React from 'react'
-import { Router, Scene } from 'react-native-router-flux'
-import BathroomPanel from './src/components/BathroomPanel.js';
+import React from 'react';
+import { Router, Scene } from 'react-native-router-flux';
 import BathroomScreen from './BathroomScreen.js';
 import BathroomPage from './src/pages/BathroomPage';
 import { StyleSheet, Text, View } from 'react-native';
 
-
-const Routes = (props) => (
-   <Router>
-      <Scene key = "root">
-         <Scene key = "home" sceneStyle={styles.container} component = {BathroomPage} title = "Home" initial = {true} />
-         <Scene key = "info" sceneStyle={styles.container} component = {BathroomScreen} title = "Bathroom Info" />
-      </Scene>  
+const Routes = () => (
+   <Router sceneStyle={styles.container}>
+      <Scene key = "root" >
+         <Scene key = "home" component = {BathroomPage} title = "Home" initial = {true} />
+         <Scene key = "info" component = {BathroomScreen} title = "Bathroom Info" />
+      </Scene>    
    </Router>   
 )
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 0,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
+        width:'100%'
     },
 }); 
 
