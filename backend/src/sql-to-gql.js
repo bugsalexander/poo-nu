be4349afbbf2191d886fe2188897e9c1b2619cfa
meebs <alex@takayamasan.com>
2019-10-20 01:06:54 -0400

    added reformatter file

---
+++ backend/src/sql-to-gql.js
@@ -0,0 +1,19 @@
+/**
+ * This file is part of the Poo-NU repository.
+ */
+ 
+export function reformatBathroom(sql_bathroom) {
+  return undefined;
+}
+
+export function reformatBathrooms(sql_bathrooms) {
+  return undefined;
+}
+
+export function reformatBuildings(sql_building) {
+  return undefined;
+}
+
+export function reformatBathroomRating(sql_rating) {
+  return undefined;
+}
